986cfb2160bf772cd2c7287de43850d32e308d22
unknown <gdkhanhnguyen2305@gmail.com>
2023-05-29 00:36:06 +0700

    Added draw_marker function which draws X and O

--- board.py
+++ board.py
@@ -3,8 +3,12 @@ import pygame
 
 width = 900
 height = 900
-
 line_width = 5
+marker_width = 8
+
+# Define colors
+green = (0, 255, 0)
+red = (255, 0, 0)
 
 screen = pygame.display.set_mode((width, height))
 
@@ -15,3 +19,21 @@ def draw_grid():
     for x in range(1, 3):
         pygame.draw.line(screen, grid, (0, x * 300), (width, x * 300), line_width)
         pygame.draw.line(screen, grid, (x * 300, 0), (x * 300, width), line_width)
+
+markers = []
+for x in range(3):
+    row = [0] * 3
+    markers.append(row)
+
+def draw_markers():
+    x_pos = 0
+    for x in markers:
+        y_pos = 0
+        for y in x:
+            if y == 1:
+                pygame.draw.line(screen, green, (x_pos * 300 + 50, y_pos * 300 + 50), (x_pos * 300 + 250, y_pos * 300 + 250), marker_width)
+                pygame.draw.line(screen, green, (x_pos * 300 + 50, y_pos * 300 + 250), (x_pos * 300 + 250, y_pos * 300 + 50), marker_width)
+            if y == -1:
+                pygame.draw.circle(screen, red, (x_pos * 300 + 150, y_pos * 300 + 150), 100, marker_width)
+            y_pos += 1
+        x_pos += 1
--- main.py
+++ main.py
@@ -5,13 +5,29 @@ pygame.init()
 
 pygame.display.set_caption("Tic Tac Toe by Tuticoi")
 
+clicked = False
 run = True
+player = 1
+
+
+
 while run:
     board.draw_grid()
+    board.draw_markers()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+        if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
+            clicked = True
+        if event.type == pygame.MOUSEBUTTONUP and clicked == True:
+            clicked = False
+            pos = pygame.mouse.get_pos()
+            cell_x = pos[0]
+            cell_y = pos[1]
+            if board.markers[cell_x // 300][cell_y // 300] == 0:
+                board.markers[cell_x // 300][cell_y // 300] = player
+                player *= -1
     pygame.display.update()
 
 pygame.quit()
